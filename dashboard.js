59e051fd87a7eea0f9eb66b6ea186523a28552f9
Birdizzle28 <31896847+Birdizzle28@users.noreply.github.com>
2025-06-02 21:30:49 -0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -3,7 +3,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 const supabase = createClient(
   'https://ddlbgkolnayqrxslzsxn.supabase.co',
-  'your-public-anon-key' // replace this with your actual anon key
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkbGJna29sbmF5cXJ4c2x6c3huIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4Mjg0OTQsImV4cCI6MjA2NDQwNDQ5NH0.-L0N2cuh0g-6ymDyClQbM8aAuldMQzOb3SXV5TDT5Ho' // replace this with your actual anon key
 );
 
 // TAB SWITCHING
