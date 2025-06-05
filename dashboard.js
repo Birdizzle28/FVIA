9e07dcd2ea47dd21f8f4747d2465f2630ff0da07
Birdizzle28 <31896847+Birdizzle28@users.noreply.github.com>
2025-06-05 01:15:55 -0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -6,9 +6,6 @@ const supabase = createClient(
 );
 
 /*alert("Step 3: Checking session from dashboard...");*/
-document.addEventListener("DOMContentLoaded", async () => {
-  alert("✅ dashboard.js is finally running!");
-});
 /*document.addEventListener("DOMContentLoaded", async () => {
   alert("✅ DOM fully loaded and JS is running.");
   const loadingScreen = document.getElementById('loading-screen');
