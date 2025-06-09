308cfa866a1f5cee1bc272c7a5037554ec764dd2
Birdizzle28 <31896847+Birdizzle28@users.noreply.github.com>
2025-06-08 19:25:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -141,7 +141,6 @@ const supabase = createClient(
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkbGJna29sbmF5cXJ4c2x6c3huIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4Mjg0OTQsImV4cCI6MjA2NDQwNDQ5NH0.-L0N2cuh0g-6ymDyClQbM8aAuldMQzOb3SXV5TDT5Ho'
 );
 
-document.addEventListener('DOMContentLoaded', async () => {
   document.addEventListener('DOMContentLoaded', async () => {
   alert("✅ SCRIPT IS RUNNING");
 
