3148109b9a1dd1c5e7c46a069d7bd3393a3cade8
Birdizzle28 <31896847+Birdizzle28@users.noreply.github.com>
2025-07-23 23:00:11 -0500

    Update maps.js

--- scripts/maps.js
+++ scripts/maps.js
@@ -5,7 +5,8 @@ const supabase = createClient(
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkbGJna29sbmF5cXJ4c2x6c3huIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4Mjg0OTQsImV4cCI6MjA2NDQwNDQ5NH0.-L0N2cuh0g-6ymDyClQbM8aAuldMQzOb3SXV5TDT5Ho'
 );
 
-let map; // must be global for callbackasync function loadLeadPins(user, isAdmin, viewMode = 'mine') {
+let map; // must be global for callback
+async function loadLeadPins(user, isAdmin, viewMode = 'mine') {
   let query = supabase.from('leads').select('*').not('lat', 'is', null).not('lng', 'is', null);
 
   if (!isAdmin || viewMode === 'mine') {
