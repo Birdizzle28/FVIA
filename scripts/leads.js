f2233357d6478ece7041e217f75e05edee910e12
Birdizzle28 <31896847+Birdizzle28@users.noreply.github.com>
2025-07-25 20:28:29 -0500

    Update leads.js

--- scripts/leads.js
+++ scripts/leads.js
@@ -111,7 +111,7 @@ function formatPhoneInput(input) {
 }
 document.querySelectorAll('input[name="lead-phone"]').forEach(formatPhoneInput);
 async function geocodeAddress(address) {
-  const apiKey = 'AIzaSyD5nGhz1mUXK1aGsoQSzo4MXYcI-uoxPa4'; // 🔁 Replace with your actual API key
+  const apiKey = 'AIzaSyCFVLjqLqrSuOcGlTJzqYRM4yrsCcaYZzA'; // 🔁 Replace with your actual API key
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`;
   console.log(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`);
   try {
