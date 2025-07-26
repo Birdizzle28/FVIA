32d1c3330cbe09125fce4b575bedb1a97992d830
Birdizzle28 <31896847+Birdizzle28@users.noreply.github.com>
2025-07-25 20:24:33 -0500

    Update leads.js

--- scripts/leads.js
+++ scripts/leads.js
@@ -1,7 +1,7 @@
 // Import Supabase client
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 const supabase = createClient('https://ddlbgkolnayqrxslzsxn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkbGJna29sbmF5cXJ4c2x6c3huIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4Mjg0OTQsImV4cCI6MjA2NDQwNDQ5NH0.-L0N2cuh0g-6ymDyClQbM8aAuldMQzOb3SXV5TDT5Ho');
-console.log(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`);
+
 let agentCurrentPage = 1;
 let agentTotalPages = 1;
 const pageSize = 25;
