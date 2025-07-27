374e3a619d0d8a0fc284674855cdfccdf4bbf3b0
Birdizzle28 <31896847+Birdizzle28@users.noreply.github.com>
2025-07-27 13:57:47 -0500

    Update chatgpt.js

--- netlify/functions/chatgpt.js
+++ netlify/functions/chatgpt.js
@@ -1,8 +1,8 @@
-const { Configuration, OpenAIApi } = require("openai");
+/*const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
-});
+});*/
 const openai = new OpenAIApi(configuration);
 
 exports.handler = async function (event) {
