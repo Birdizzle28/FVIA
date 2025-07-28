9602d303c0fb3de08c0e4d80d83cd2afe3b4a834
Birdizzle28 <31896847+Birdizzle28@users.noreply.github.com>
2025-07-27 23:22:24 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,14 @@ const supabase = createClient(
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkbGJna29sbmF5cXJ4c2x6c3huIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4Mjg0OTQsImV4cCI6MjA2NDQwNDQ5NH0.-L0N2cuh0g-6ymDyClQbM8aAuldMQzOb3SXV5TDT5Ho'
 );
 
+const hamburger = document.getElementById('hamburger');
+const navMenu   = document.querySelector('.header-flex-container');
+
+hamburger.addEventListener('click', () => {
+  // Toggle the 'open' class on the nav menu to show or hide it
+  navMenu.classList.toggle('open');
+});
+
 window.addEventListener("load", () => {
   const slides = document.querySelector(".carousel");
   const thumbWrapper = document.querySelector(".thumbnail-wrapper");
