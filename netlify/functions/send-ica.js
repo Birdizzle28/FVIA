1f7f029ddf3662763de4d746663dad855925e759
Birdizzle28 <31896847+Birdizzle28@users.noreply.github.com>
2025-11-19 01:40:01 -0600

    Update send-ica.js

--- netlify/functions/send-ica.js
+++ netlify/functions/send-ica.js
@@ -74,7 +74,7 @@ async function createEnvelopeWithEsignProvider(payload) {
       {
         name: `${payload.firstName} ${payload.lastName}`,
         email: payload.email,
-        role: signerRole
+        id: signerRole
       }
     ],
     // Optional: store extra info on the document
