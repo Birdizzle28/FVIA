37062e648167811d341aeace1055f99ce744d9b5
Birdizzle28 <31896847+Birdizzle28@users.noreply.github.com>
2025-11-19 23:41:07 -0600

    Update send-ica.js

--- netlify/functions/send-ica.js
+++ netlify/functions/send-ica.js
@@ -68,7 +68,7 @@ async function createEnvelopeWithEsignProvider(payload) {
   
   const body = {
     // use test_mode: true until you're happy
-    test_mode: false,
+    test_mode: true,
 
     // tell SignWell which template
     template_id: ESIGN_ICA_TEMPLATE_ID,
