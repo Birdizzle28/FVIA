6d0bce6c278b1a1ece10db7cbf6594d418461024
Birdizzle28 <31896847+Birdizzle28@users.noreply.github.com>
2025-11-21 22:46:03 -0600

    Update processPolicyCommission.js

--- netlify/functions/processPolicyCommission.js
+++ netlify/functions/processPolicyCommission.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
   'https://ddlbgkolnayqrxslzsxn.supabase.co',
-  'YOUR_REAL_ANON_KEY_HERE'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkbGJna29sbmF5cXJ4c2x6c3huIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4Mjg0OTQsImV4cCI6MjA2NDQwNDQ5NH0.-L0N2cuh0g-6ymDyClQbM8aAuldMQzOb3SXV5TDT5Ho'
 );
 
 export async function handler(event) {
