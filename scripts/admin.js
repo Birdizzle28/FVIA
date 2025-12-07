880330e53e98a067fd39ed7dbd50516cf4ea6d56
Birdizzle28 <31896847+Birdizzle28@users.noreply.github.com>
2025-12-07 13:24:54 -0600

    Update admin.js

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,8 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-const supabase = createClient(
-  'https://ddlbgkolnayqrxslzsxn.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkbGJna29sbmF5cXJ4c2x6c3huIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4Mjg0OTQsImV4cCI6MjA2NDQwNDQ5NH0.-L0N2cuh0g-6ymDyClQbM8aAuldMQzOb3SXV5TDT5Ho'
-);
+const supabase = window.supabaseClient;
 
 let statPicker;
 let userId = null;
