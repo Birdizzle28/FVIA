33445d596833329338010b53386b495f339e4423
Birdizzle28 <31896847+Birdizzle28@users.noreply.github.com>
2025-12-07 20:10:50 -0600

    Update maps.js

--- scripts/maps.js
+++ scripts/maps.js
@@ -1,9 +1,5 @@
 // Initialize Supabase client
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-const supabase = createClient(
-  'https://ddlbgkolnayqrxslzsxn.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkbGJna29sbmF5cXJ4c2x6c3huIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4Mjg0OTQsImV4cCI6MjA2NDQwNDQ5NH0.-L0N2cuh0g-6ymDyClQbM8aAuldMQzOb3SXV5TDT5Ho'
-);
+const supabase = window.supabaseClient;
 
 let map;                    // Google Map instance
 let directionsService;      // Google Maps Directions service
