07771f1292b7de5de8ed8eaf4a945c15c976d5c0
Birdizzle28 <31896847+Birdizzle28@users.noreply.github.com>
2025-12-08 15:33:20 -0600

    Update auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,9 +1,4 @@
 // scripts/auth.js
-const SUPABASE_URL = 'https://ddlbgkolnayqrxslzsxn.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkbGJna29sbmF5cXJ4c2x6c3huIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4Mjg0OTQsImV4cCI6MjA2NDQwNDQ5NH0.-L0N2cuh0g-6ymDyClQbM8aAuldMQzOb3SXV5TDT5Ho';
-
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
 const redirectIfNotLoggedIn = async () => {
   const { data: { user } } = await supabase.auth.getUser();
   if (!user) window.location.href = '/login.html';
