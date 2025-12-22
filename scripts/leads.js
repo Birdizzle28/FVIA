1d0927d4410baa3933b3d4d6a58e898f6bb5460a
Birdizzle28 <31896847+Birdizzle28@users.noreply.github.com>
2025-12-21 19:30:11 -0600

    Change address_line1 to address in leads.js

--- scripts/leads.js
+++ scripts/leads.js
@@ -527,7 +527,7 @@ async function loadAgentLeads() {
       <td>${l.last_name || ""}</td>
       <td>${l.age ?? ""}</td>
       <td>${(l.phone || []).join(", ")}</td>
-      <td>${l.address_line1 || ""}</td>
+      <td>${l.address || ""}</td>
       <td>${l.city || ""}</td>
       <td>${l.state || ""}</td>
       <td>${l.zip || ""}</td>
