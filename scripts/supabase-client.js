f40625e0894199d71308fdd34c9077e867b333af
Birdizzle28 <31896847+Birdizzle28@users.noreply.github.com>
2026-03-03 19:57:30 -0600

    Update supabase-client.js

--- scripts/supabase-client.js
+++ scripts/supabase-client.js
@@ -2,6 +2,7 @@
 
 const SUPABASE_URL = 'https://ddlbgkolnayqrxslzsxn.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkbGJna29sbmF5cXJ4c2x6c3huIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4Mjg0OTQsImV4cCI6MjA2NDQwNDQ5NH0.-L0N2cuh0g-6ymDyClQbM8aAuldMQzOb3SXV5TDT5Ho';
+window.VAPID_PUBLIC_KEY = "BD2IGcWhq7PWFauNfIAkQdh44nwKycEs2RMlHjk-ypuWsW4jdmoPy7_sDyHuYkMqKCMg1D0wZpc8QjRWKXfOCW0";
 
 if (!window.supabase) {
   console.error('Supabase UMD bundle not loaded before supabase-client.js');
